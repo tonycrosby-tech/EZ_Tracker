3d995c52fc4a31a37be0fd799a51f4c191767874
Mario Thompson <MarioThompson0010@gmail.com>
2021-02-20 15:24:18 -0600

    implement login

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -6,7 +6,8 @@ const isAuthenticated = require('../../config/middleware/isAuthenticated');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-router.post("/register", function(req, res) {
+// api/auth/register
+router.post("/register",  function(req, res) {
     if (!req.body.username || !req.body.password || !req.body.email) {
         res.json({success: false, msg: 'Please pass username and password.'});
     } else {
@@ -41,22 +42,25 @@ router.post('/login', function(req, res) {
     else
     {
       //validPassword
-      console.log(user);
-      user.validPassword(password, (err, same)  => {
-        if (err)
-        {
-          res.status(500);
-        }
-        else if (!same) 
-        {
-          res.status(401).json('incorrect email or password');
-        }
-        const { payload } = username;
-        const token = jwt.sign(payload, secret, {
-          expiresIn: '1h'
-        });
-        res.cookie('token', token, { httpOnly: true }).sendStatus(200);
-      })
+      // console.log(user);
+      // //user.validPassword(password, (err, same)  => {
+      //   if (err)
+      //   {
+      //     res.status(500);
+      //   }
+      //   else if (!same) 
+      //   {
+      //     res.status(401).json('incorrect email or password');
+      //   }
+      //   const { payload } = username;
+      //   const token = jwt.sign(payload, secret, {
+      //     expiresIn: '1h'
+      //   });
+      //   res.cookie('token', token, { httpOnly: true }).sendStatus(200);
+      //})
+      const { password, ...userloc } = user;
+      res.json(userloc);
+      //res.json(user);
     }
   });
 });
