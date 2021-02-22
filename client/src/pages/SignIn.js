569d0f1714408415a3e5a1a126d8bf57e6071596
tony913 <tonycrosby96@gmail.com>
2021-02-21 21:03:38 -0600

    pushing up frontend changes

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -159,55 +159,58 @@
 //   );
 // }
 
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import Link from '@material-ui/core/Link';
 
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 // import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
 // import './Login.css';
 
 class Login extends Component {
-
   constructor() {
     super();
     this.state = {
-      username: '',
-      password: '',
-      message: ''
+      username: "",
+      password: "",
+      message: "",
     };
   }
   onChange = (e) => {
-    const state = this.state
+    const state = this.state;
     state[e.target.name] = e.target.value;
     this.setState(state);
-  }
+  };
 
   onSubmit = (e) => {
     e.preventDefault();
 
     const { username, password } = this.state;
 
-    axios.post('/api/auth/login', { username, password })
+    axios
+      .post("/api/auth/login", { username, password })
       .then((result) => {
-        this.setState({ message: '' });
-        this.props.history.push('/');
+        this.setState({ message: "" });
+        this.props.history.push("/home");
       })
       .catch((error) => {
-        this.setState({ message: 'Login failed. Username or password do not match' });
+        this.setState({
+          message: "Login failed. Username or password do not match",
+        });
       });
-  }
+  };
+
 
   render() {
     const { username, password, message } = this.state;
@@ -230,79 +233,77 @@ class Login extends Component {
       //     </p>
       //   </form>
       // </div>
-          <Grid container component="main" >
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7}/>
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div >
-          <Avatar >
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form onSubmit={this.onSubmit}>
-            {message !== '' &&
-              <p>{ message }</p>
-          }
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Username"
-              name="username"
-              value={username}
-              autoFocus
-              onChange={this.onChange} required
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              value={password}
-              id="password"
-              autoComplete="current-password"
-              onChange={this.onChange} required
-            />
-            <Grid container justify="flex-end">
-              <Grid item>
-                <Link href="/">
-                  Don't Have an Account? Sign up
-                </Link>
+      <Grid container component="main">
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} />
+        <Grid item xs={12} sm={8} md={5} elevation={6} square>
+          <div>
+            <Avatar>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <form onSubmit={this.onSubmit}>
+              {message !== "" && <p>{message}</p>}
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                value={username}
+                autoFocus
+                onChange={this.onChange}
+                required
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                value={password}
+                id="password"
+                autoComplete="current-password"
+                onChange={this.onChange}
+                required
+              />
+              <Grid container justify="flex-end">
+                <Grid item>
+                  <Link href="/">Don't Have an Account? Sign up</Link>
+                </Grid>
               </Grid>
-            </Grid>
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-            </Grid>
-            {/* <Box mt={5}>
+              {/* <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              /> */}
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+              >
+                Sign In
+              </Button>
+              {/* <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+              </Grid> */}
+              {/* <Box mt={5}>
               <Copyright />
             </Box> */}
-          </form>
-        </div>
+            </form>
+          </div>
+        </Grid>
       </Grid>
-    </Grid>
     );
   }
 }
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,148 +1,4 @@
 // import React, { useState } from "react";
-// import Avatar from "@material-ui/core/Avatar";
-// import Button from "@material-ui/core/Button";
-// import CssBaseline from "@material-ui/core/CssBaseline";
-// import TextField from "@material-ui/core/TextField";
-// import Link from "@material-ui/core/Link";
-// import Grid from "@material-ui/core/Grid";
-// import Box from "@material-ui/core/Box";
-// import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-// import Typography from "@material-ui/core/Typography";
-// import { makeStyles } from "@material-ui/core/styles";
-// import Container from "@material-ui/core/Container";
-// import API from "../utils/API";
-
-// const useStyles = makeStyles((theme) => ({
-//   paper: {
-//     marginTop: theme.spacing(8),
-//     display: "flex",
-//     flexDirection: "column",
-//     alignItems: "center",
-//   },
-//   avatar: {
-//     margin: theme.spacing(1),
-//     backgroundColor: theme.palette.primary.main,
-//   },
-//   form: {
-//     width: "90%", // Fix IE 11 issue.
-//     marginTop: theme.spacing(3),
-//   },
-//   submit: {
-//     margin: theme.spacing(3, 0, 2),
-//     backgroundColor: "#00008b",
-//     color: "white",
-//   },
-// }));
-
-// export default function SignUp() {
-//   const classes = useStyles();
-//   const [formObject, setFormObject] = useState({});
-
-//   function handleInputChange(event) {
-//     const { name, value } = event.target;
-//     setFormObject({ ...formObject, [name]: value });
-//   }
-
-//   function handleSubmit(e) {
-//     e.preventDefault();
-//     if (formObject.user && formObject.email && formObject.password) {
-//       API.saveUser({
-//         username: formObject.user,
-//         email: formObject.email,
-//         password: formObject.password,
-//       })
-//         .then((res) => {
-//           console.log(res);
-//         })
-//         .catch((err) => console.log(err));
-//     }
-//   };
-
-//   return (
-//     <Container component="main" maxWidth="xs">
-//       <CssBaseline />
-//       <div className={classes.paper}>
-//         <Avatar className={classes.avatar}>
-//           <LockOutlinedIcon />
-//         </Avatar>
-//         <Typography component="h1" variant="h5">
-//           Sign up
-//         </Typography>
-//         <form className={classes.form} onSubmit={handleSubmit}>
-//           <Grid container spacing={2}>
-//             <Grid item xs={12}>
-//               <TextField
-//                 variant="outlined"
-//                 margin="normal"
-//                 required
-//                 fullWidth
-//                 id="username"
-//                 label="Username"
-//                 name="user"
-//                 autoComplete="username"
-//                 autoFocus
-//                 onChange={handleInputChange}
-//               />
-//             </Grid>
-//             <Grid item xs={12}>
-//               <TextField
-//                 variant="outlined"
-//                 required
-//                 fullWidth
-//                 id="user"
-//                 label="Email"
-//                 name="email"
-//                 autoComplete="email"
-//                 onChange={handleInputChange}
-//               />
-//             </Grid>
-//             <Grid item xs={12}>
-//               <TextField
-//                 variant="outlined"
-//                 required
-//                 fullWidth
-//                 name="password"
-//                 label="Password"
-//                 type="password"
-//                 id="password"
-//                 autoComplete="current-password"
-//                 onChange={handleInputChange}
-//               />
-//             </Grid>
-//             <Button type="submit" className={classes.submit} fullWidth>
-//               Sign Up
-//             </Button>
-//             <Grid container justify="flex-end">
-//               <Grid item>
-//                 <Link href="/login" variant="body1">
-//                   Already have an account? Sign in
-//                 </Link>
-//               </Grid>
-//             </Grid>
-//           </Grid>
-//         </form>
-//         <Box mt={2} mb={2}>
-//           <Copyright />
-//         </Box>
-//       </div>
-//     </Container>
-//   );
-// }
-
-// function Copyright() {
-//   return (
-//     <Typography variant="body2" color="textSecondary" align="center">
-//       {"Copyright © "}
-//       <Link color="inherit" href="#">
-//         Subscription Tracker
-//       </Link>{" "}
-//       {new Date().getFullYear()}
-//       {"."}
-//     </Typography>
-//   );
-// }
-
-// import React, { useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -158,30 +14,9 @@ import API from "../utils/API";
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-import { Link } from "react-router-dom";
-// import './Login.css';
+import Link from '@material-ui/core/Link';
 
-// const useStyles = makeStyles((theme) => ({
-//   paper: {
-//     marginTop: theme.spacing(8),
-//     display: "flex",
-//     flexDirection: "column",
-//     alignItems: "center",
-//   },
-//   avatar: {
-//     margin: theme.spacing(1),
-//     backgroundColor: theme.palette.primary.main,
-//   },
-//   form: {
-//     width: "90%", // Fix IE 11 issue.
-//     marginTop: theme.spacing(3),
-//   },
-//   submit: {
-//     margin: theme.spacing(3, 0, 2),
-//     backgroundColor: "#00008b",
-//     color: "white",
-//   },
-// }));
+
 class Signup extends Component {
   constructor() {
     super();
@@ -287,7 +122,7 @@ class Signup extends Component {
                   required
                 />
               </Grid>
-              <Button type="submit" fullWidth>
+              <Button type="submit" variant='contained' color='primary' fullWidth>
                 Sign Up
               </Button>
               <Grid container justify="flex-end">
