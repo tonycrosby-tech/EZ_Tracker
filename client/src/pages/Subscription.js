1681bbe07552750390533500eacfa33e753c0051
tony913 <tonycrosby96@gmail.com>
2021-02-21 23:09:22 -0600

    adding subscription no functionality yet

--- client/src/pages/Subscription.js
+++ client/src/pages/Subscription.js
@@ -2,22 +2,20 @@ import React, { useState } from "react";
 import { DataGrid } from "@material-ui/data-grid";
 import Calendar from "react-calendar";
 import { makeStyles } from "@material-ui/core/styles";
-// import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-// import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-// import CardActions from '@material-ui/core/CardActions';
-// import Collapse from '@material-ui/core/Collapse';
+import List from "@material-ui/core/List";
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-// import FavoriteIcon from '@material-ui/icons/Favorite';
-// import ShareIcon from '@material-ui/icons/Share';
-// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import Grid from '@material-ui/core/Grid';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Grid from "@material-ui/core/Grid";
 import "react-calendar/dist/Calendar.css";
 
 const columns = [
@@ -28,7 +26,6 @@ const columns = [
       "This column is for the id to keep count of your subscriptions.",
     sortable: false,
     width: 100,
-    // valueGetter: (params) => `${params.getValue("id")}`,
   },
   {
     field: "name",
@@ -36,7 +33,6 @@ const columns = [
     description: "This column is for the name of your subscription.",
     sortable: false,
     width: 300,
-    // valueGetter: (params) => `${params.getValue("name")}`,
   },
   {
     field: "price",
@@ -44,7 +40,6 @@ const columns = [
     description: "This column is for the price of your subscription.",
     sortable: false,
     width: 150,
-    // valueGetter: (params) => `${params.getValue("price")}`,
   },
   {
     field: "expiration",
@@ -52,7 +47,6 @@ const columns = [
     description: "This column is for when your subscription expires.",
     sortable: false,
     width: 250,
-    // valueGetter: (params) => `${params.getValue('expiration')}`,
   },
 ];
 
@@ -66,17 +60,17 @@ const useStyles = makeStyles((theme) => ({
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%",
   },
   expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
       duration: theme.transitions.duration.shortest,
     }),
   },
   expandOpen: {
-    transform: 'rotate(180deg)',
+    transform: "rotate(180deg)",
   },
   avatar: {
     backgroundColor: red[500],
@@ -90,43 +84,54 @@ const Subscription = () => {
 
   return (
     <div>
-    <h1>Subscriptions</h1>
-    <Grid direction="column" justify="left">
-      <div style={{ height: 300, width: "50%" }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={4}
-          checkboxSelection
-        />
-      </div>
+      <h1>Subscriptions</h1>
+      <Grid direction="column" justify="left">
+        <div style={{ height: 300, width: "50%" }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={4}
+            checkboxSelection
+          />
+        </div>
       </Grid>
 
       <div>
-      <Grid direction="column" justify="left">
-        <Card className={classes.root}>
-          <CardHeader
-            avatar={
-              <Avatar aria-label="recipe" className={classes.avatar}>
-                N
-              </Avatar>
-            }
-            action={
-              <IconButton aria-label="settings">
-                <MoreVertIcon />
-              </IconButton>
-            }
-            title="Expiration for Netflix"
-            subheader="February 21, 2020"
-          />
-          <Calendar onChange={onChange} value={value} />
-          <CardContent>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Your Subscription is set to expire: Februrary 21, 2021
-            </Typography>
-          </CardContent>
-        </Card>
+        <Grid direction="column" justify="left">
+          <Card className={classes.root}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="recipe" className={classes.avatar}>
+                  N
+                </Avatar>
+              }
+              action={
+                <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title="Expiration for Netflix"
+              subheader="February 21, 2020"
+            />
+            <Calendar onChange={onChange} value={value} />
+            <CardContent>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Your Subscription is set to expire: Februrary 21, 2021
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
+      </div>
+
+      <div>
+      <List className={classes.root}>
+      <ListItem>
+        <ListItemAvatar>
+            <AddCircleIcon />
+        </ListItemAvatar>
+        <ListItemText primary="Add a new Subscription" />
+      </ListItem>
+      </List>
       </div>
     </div>
   );
