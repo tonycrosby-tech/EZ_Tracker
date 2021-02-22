1654aa7232a53a3b7ad8fc70d5e4e8ed1ea0b80c
Mario Thompson <MarioThompson0010@gmail.com>
2021-02-22 11:53:05 -0600

    added seeder for quick seeding of local and or Heroku database

---
+++ seed/seeder.js
@@ -0,0 +1,6 @@
+//use tracker; // may not need this
+db.subscriptions.insertMany([
+    {"name":"Netflix", "cost":10, "satisfaction":5, "dateCreated":"2020-02-22 00:00:00.000Z", "__v":"0"},
+    {"name":"Amazon Prime", "cost":50, "satisfaction":5, "dateCreated":"2020-02-22 00:00:00.000Z", "__v":"0"},
+    {"name":"Disney Plus", "cost":20, "satisfaction":5, "dateCreated":"2020-02-22 00:00:00.000Z", "__v":"0"}
+  ]);
