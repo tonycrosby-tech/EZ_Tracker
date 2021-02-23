e0d4c5ce7964ffd015da50ef35907a17b8aa38c9
Mario Thompson <MarioThompson0010@gmail.com>
2021-02-22 20:12:10 -0600

    add expiration date

--- server/models/Subscription.js
+++ server/models/Subscription.js
@@ -7,6 +7,7 @@ const subscriptionSchema = new Schema({
     name: {type: String, required: true, unique: false},
     cost: {type: Number, required: false, unique: false},
     satisfaction: {type: Number, required: false, unique: false},
+    expirationDate: {type: Date, required: false, unique: false},
     dateCreated: {type: Date, default: Date.now},
 });
 
