7aa14d6697ac79bd4463bf6ddfeeaf29689e6b1c
tony913 <tonycrosby96@gmail.com>
2021-02-23 17:04:25 -0600

    fixed signup and signin pages

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,19 +3,34 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-// import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import API from "../utils/API";
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import axios from "axios";
 import Link from "@material-ui/core/Link";
 
+const styles = theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: "#00008b",
+  },
+  form: {
+    width: '90%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+});
 class Signup extends Component {
   constructor() {
     super();
@@ -50,18 +65,19 @@ class Signup extends Component {
   };
 
   render() {
+    const { classes } = this.props;
     const { email, username, password } = this.state;
     return (
       <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <div>
-          <Avatar>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography component="h1" variant="h5">
+          <Typography  component="h1" variant="h5">
             Sign up
           </Typography>
-          <form onSubmit={this.onSubmit}>
+          <form className={classes.form} onSubmit={this.onSubmit}>
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <TextField
@@ -113,6 +129,7 @@ class Signup extends Component {
                 variant="contained"
                 color="primary"
                 fullWidth
+                className={classes.submit}
               >
                 Sign Up
               </Button>
@@ -134,4 +151,4 @@ class Signup extends Component {
   }
 }
 
-export default Signup;
+export default withStyles(styles, { withTheme: true })(Signup);
