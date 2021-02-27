51cdd6a455a2132a06a30890f4ccbdd46fff7270
tony913 <tonycrosby96@gmail.com>
2021-02-27 11:50:19 -0600

    redoing bargraph and piegraph

--- client/src/components/PieGraph.js
+++ client/src/components/PieGraph.js
@@ -1,54 +1,82 @@
-import React from 'react';
-import { Pie } from 'react-chartjs-2';
-import axios from 'axios';
+import React from "react";
+import { Pie } from "react-chartjs-2";
+import axios from "axios";
 
-axios.get('/api/auth/getAllSubs').then((res) => {
-  const subs = res.data.subscriptions;
-
-  if (subs) {
-    subs.map((item) => {
-      let joy = item.rating;
-      let name = item.SubscriptionName;
-      enjoyment.push(joy);
-      title.push(name);
-    });
-  }
-});
-
-const enjoyment = [];
-const title = [];
-
-const state = {
-  labels: title,
-  datasets: [
-    {
-      label: 'Enjoyment',
-      backgroundColor: ['#B21F00', '#C9DE00', '#2FDE00', '#00A6B4', '#6800B4'],
-      hoverBackgroundColor: [
-        '#501800',
-        '#4B5000',
-        '#175000',
-        '#003350',
-        '#35014F',
-      ],
-      data: enjoyment,
-    },
-  ],
-};
+// const state = {
+//   labels: title,
+//   datasets: [
+//     {
+//       label: 'Enjoyment',
+//       backgroundColor: ['#B21F00', '#C9DE00', '#2FDE00', '#00A6B4', '#6800B4'],
+//       hoverBackgroundColor: [
+//         '#501800',
+//         '#4B5000',
+//         '#175000',
+//         '#003350',
+//         '#35014F',
+//       ],
+//       data: enjoyment,
+//     },
+//   ],
+// };
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subscriptions: [],
+    };
+  }
+
+  groomGraphData() {
+    const labels = this.state.subscriptions.map(
+      (item) => item.SubscriptionName
+    );
+    const data = this.state.subscriptions.map((item) => item.rating);
+
+    return {
+      labels,
+      datasets: [
+        {
+          label: "Enjoyment",
+          backgroundColor: [
+            "#B21F00",
+            "#C9DE00",
+            "#2FDE00",
+            "#00A6B4",
+            "#6800B4",
+          ],
+          hoverBackgroundColor: [
+            "#501800",
+            "#4B5000",
+            "#175000",
+            "#003350",
+            "#35014F",
+          ],
+          data,
+        },
+      ],
+    };
+  }
+
+  componentDidMount() {
+    axios.get("/api/auth/getAllSubs").then((res) => {
+      this.setState({ subscriptions: res.data.subscriptions });
+    });
+  }
+
   render() {
     return (
       <div>
         <Pie
-          data={state}
+          data={this.groomGraphData()}
           width={350}
           height={300}
           options={{
             maintainAspectRatio: false,
             title: {
               display: true,
-              text: 'Average Enjoyment of Subs',
+              text: "Average Enjoyment of Subs",
               fontSize: 20,
             },
           }}
